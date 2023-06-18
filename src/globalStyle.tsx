8d930774ec0330c448a78be77eefb83e68d6b756
Iago <iagocesarts@hotmail.com>
2023-06-17 23:38:30 -0300

    Pequena organização do globalStyles.

--- src/globalStyle.tsx
+++ src/globalStyle.tsx
@@ -7,12 +7,13 @@ export const global = {
         laranja: '#fd720f',
         cinza: '#3E3E3E',
         cinzaTexto: 'rgb(0, 0, 0, .49)',
+        desativado: '#C2C3C4',
+/*---------------------------------------------------- */
         cinzaTitulo: '#818181',
         cinza1: '#f4f4f4',
         cinza2: '#d9d9d9',
         cinza3: '#cccccc',
-        cinzaOF: '#E3E3E3',
-        desativado: '#C2C3C4'
+        cinzaOF: '#E3E3E3'
     },
     fonts: {
         bebas: 'Bebas Neue',
