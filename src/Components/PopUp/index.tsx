99a9b690eaa2c78769545d261d7ba70f9b42a0ea
carloseduardobanjar <carlosesb@dcc.ufrj.br>
2023-07-31 12:40:56 -0300

    Opção de ocultar popup

--- src/Components/PopUp/index.tsx
+++ src/Components/PopUp/index.tsx
@@ -1,5 +1,6 @@
 import { PopText, PopupDiv } from "./style";
 import Download from '../../Assets/Cardapio/download.svg';
+import { useState } from "react";
 
 /*
     FICOM COM DEUS PORQUE ATÉ ENTÃO NÃO DÁ PRA INSTALAR PELO PRÓPRIO POPUP
@@ -7,10 +8,12 @@ import Download from '../../Assets/Cardapio/download.svg';
 */
 
 export default function DownPop() {
+    const [visibility, setVisibility] = useState(true)
     return(
-        <PopupDiv>
+        <PopupDiv onClick={() => setVisibility(false)} style={{display: (visibility) ? '' : 'none'}}>
             <PopText>
                 Instale esse aplicativo no seu Iphone: aperte <img width="15" height="15" src={Download} alt="Icon tap"></img> e depois Adicionar à tela inicial.
+                <br/><i>Clique para ocultar</i>.
             </PopText>
         </PopupDiv>
     );
