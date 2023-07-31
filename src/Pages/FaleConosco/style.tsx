b0cf7989d20ee47a293bba82402fc19c2932da29
Iago <iagocesarts@hotmail.com>
2023-07-30 21:36:18 -0300

    Adição das cores dos fundos.

--- src/Pages/FaleConosco/style.tsx
+++ src/Pages/FaleConosco/style.tsx
@@ -8,6 +8,8 @@ export const FaleDiv = styled.div `
     min-height: 100vh;
 
     align-items: center;
+
+    background: ${global.colors.fundo};
 `;
 
 /*----------------------------------------------------------------------------*/
@@ -24,6 +26,8 @@ export const BalaoInfo = styled.section `
     
     /* Lembrar de tirar isso depois, foi só pq o fundo tá branco */
     box-shadow: 0 3px 5px 1px rgba(0, 0, 0, 0.3);
+
+    background: ${global.colors.branco};
     
     div {
         display: grid;
