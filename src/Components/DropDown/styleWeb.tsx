eacca3daf7b3c2eb62fcc9f23e4a000bb237aa29
unknown <raphafm.rf@gmail.com>
2024-01-23 19:58:00 -0300

    estilizacao do dropdown especifica para web

---
+++ src/Components/DropDown/styleWeb.tsx
@@ -0,0 +1,86 @@
+import styled from "styled-components";
+import { global } from "../../globalStyle";
+
+/*
+    Correção da primeira transição da animação.
+    https://stackoverflow.com/questions/67732010/css-transition-is-not-working-for-the-first-time
+*/
+
+export const DropDiv = styled.div `
+    display: flex;
+    flex-direction: column;
+    padding: 0;
+
+    align-items: center;
+    overflow-y: hidden;
+
+    width: 100%;
+`;
+
+export const Selecionado = styled.div `
+    box-sizing: border-box;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    height: 4.6875vw;
+    width: 100%;
+    
+    background: ${global.colors.branco};
+    border: 2px solid ${global.colors.cinzaOpaco(0.24)};
+    border-radius: 16px;
+
+    button {
+        padding-left: 1vw;
+    }
+`;
+
+export const IconeEsquerda = styled.img `
+    width: 2.48vw;
+    margin-left: 6.1vw;
+`;
+
+export const DropArrow = styled.img `
+    width: 3.33vw;
+    margin-right: 6.1vw;
+`;
+
+export const Opcoes = styled.div `
+    box-sizing: border-box;
+    display: block;
+    height: 0;
+    width: 100%;
+
+    z-index: 1;
+
+    opacity: 0;
+    pointer-events: none;
+    
+    overflow: hidden;
+
+    transform: translateY(-7.5vh);
+    transition: opacity 300ms ease-in-out, transform 300ms ease-in-out, height 300ms ease-in-out;
+
+    background: ${global.colors.branco};
+    border: 2px solid ${global.colors.cinzaOpaco(0.24)};
+    border-radius: 16px;
+`;
+
+export const DropItem = styled.button `
+    display: flex;
+    flex-direction: row;
+    padding-left: 3.6vw;
+    align-items: center;
+
+    width: 100%;
+    height: 4.6875vw;
+
+    z-index: 1;
+
+    font-size: 1.25vw;
+    font-family: ${global.fonts.quickSand};
+    font-weight: 700;
+
+    border: none;
+    
+    color: ${global.colors.cinza};
+`;
